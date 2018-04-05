4ce50d3e94a1405ca569080993b16b01a68b9ee1
khajahussain1 <poolakhaja@gmail.com>
2018-04-05 17:08:42 +0530

    deleting java files

--- src/p/Constant.java
+++
@@ -1,8 +0,0 @@
-package p;
-
-public class Constant 
-{
-	public static final String URl = "D:\\Work_Spaca_Oxyzen\\SELENIUM2\\lib\\chromedriver.exe";
-	public static final String SPICEJET_URl = "http://www.spicejet.com/";
-
-}
