a19d9694fe415006a1c958815ae271576d3b6f19
khajahussain1 <poolakhaja@gmail.com>
2018-05-08 20:13:17 +0530

    added all

--- src/utility/Constant.java
+++ src/utility/Constant.java
@@ -8,7 +8,7 @@ public class Constant
 
     public static final String Password = "aish@1234$$";
 
-    public static final String Path_TestData = "F:\\Workspace_Seetest\\SELENIUM\\src\\testData\\TestData.xlsx";
+    public static final String Path_TestData = "C:\\Users\\Hussain\\Desktop\\Git _Local _Repo\\SELENIUM2\\src\\testData\\TestData.xlsx";
 
     public static final String File_TestData = "TestData.xlsx";
 	
